ccf6db1a6d3d1f1a285e8c2c4e52c99de4e6b1d8
Daniel Roebuck <danielroebuck@mac.com>
2011-06-29 20:45:16 +0100

    Added trainer and readLine of user.

--- src/main/scala/pgame/PGame.scala
+++ src/main/scala/pgame/PGame.scala
@@ -2,9 +2,14 @@ package pgame
 
 object PGame extends App {
     
-  println("Welcome to Pokemon Distortion!")
+  println("Welcome to Pokemon Master Quest!")
 
+  val daniel = Trainer("daniel")
   val e = Pokemon("Elemental", Set(Attack.ELEMENTAL_PYLON), Set(PokemonType.ELEMENTAL))
 
-  println(e)
+  val name = readLine("Type in your name: ")
+
+  val trainer = if (name == daniel.name) daniel else null
+
+  println("Welcome: " + trainer)
 }
---
+++ src/main/scala/pgame/Trainer.scala
@@ -0,0 +1,11 @@
+package pgame
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: daniel
+ * Date: 29/06/2011
+ * Time: 20:00
+ * To change this template use File | Settings | File Templates.
+ */
+
+case class Trainer(name: String)
