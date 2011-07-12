623d0cc0d6d3b5c68d4351d1051dde641240709e
Daniel Roebuck <danielroebuck@mac.com>
2011-07-12 20:22:03 +0100

    Holiday day 2

--- src/main/scala/pgame/PGame.scala
+++ src/main/scala/pgame/PGame.scala
@@ -4,10 +4,14 @@ object PGame extends App {
     
   println("Welcome to Pokemon Master Quest!")
 
-  val e = Pokemon("Elemental", Set(Attack.ELEMENTAL_PYLON), Set(PokemonType.ELEMENTAL))
-  val daniel = Trainer("daniel", Set(e))
+  val iceDragon = Pokemon("Kyuri", Set(Attack.FLY, Attack.ICE_BLADE, Attack.PROTECT, Attack.ICE_BEAM),
+    Set(PokemonType.ICE, PokemonType.DRAGON, PokemonType.FLYING), "Kyuro")
 
-  val je = Pokemon("Jelemental", Set(Attack.ELEMENTAL_RING), Set(PokemonType.ELEMENTAL))
+  val e = Pokemon("Elemental", Set(Attack.ELEMENTAL_PYLON, Attack.ELEMENTAL_BOLT, Attack.PROTECT, Attack.ELEMENTAL_BLAST), Set(PokemonType.ELEMENTAL), "Elemental")
+  val daniel = Trainer("daniel", Set(e, iceDragon))
+
+  val je = Pokemon("Jelemental", Set(Attack.ELEMENTAL_RING, Attack.ELEMENTAL_PAW, Attack.BUSHY_TAIL,
+    Attack.HEAD_BUT), Set(PokemonType.ELEMENTAL), "Jelemental")
   val joe = Trainer("joe", Set(je))
 
   val name = readLine("Type in your name: ")
